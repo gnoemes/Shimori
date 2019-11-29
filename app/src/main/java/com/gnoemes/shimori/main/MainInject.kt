88dea1e2a33c0cad562a936a59227594dd963395
Gnoemes <gnoemes@gmail.com>
2019-11-29 23:06:53 +0400

    removed unnecessary view model builder

--- app/src/main/java/com/gnoemes/shimori/main/MainInject.kt
+++ app/src/main/java/com/gnoemes/shimori/main/MainInject.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.lifecycle.ViewModel
 import com.gnoemes.common.di.ViewModelKey
 import com.gnoemes.shimori.base.di.PerActivity
-import com.gnoemes.shimori.di.ViewModelBuilder
 import com.gnoemes.shimori.search.SearchBuilder
 import dagger.Binds
 import dagger.Module
@@ -14,7 +13,6 @@ import dagger.multibindings.IntoMap
 @Module
 abstract class MainBuilder {
     @ContributesAndroidInjector(modules = [
-        ViewModelBuilder::class,
         SearchBuilder::class
     ])
     abstract fun mainAcitivity(): MainActivity
