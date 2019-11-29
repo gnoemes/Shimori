45b85bf18381ec11eb72c8194b164f7992f4d27e
Gnoemes <gnoemes@gmail.com>
2019-11-29 22:23:31 +0400

    updated dependencies

--- buildSrc/src/main/java/com/gnoemes/shimori/dependencies.kt
+++ buildSrc/src/main/java/com/gnoemes/shimori/dependencies.kt
@@ -16,11 +16,6 @@ object Modules {
     const val featureSearch = ":features:search"
 }
 
-object Releases {
-    const val versionCode = 1
-    const val versionName = "1.0"
-}
-
 object Versions {
     const val compileSdk = 29
     const val minSdk = 16
@@ -30,7 +25,7 @@ object Versions {
 }
 
 object Libs {
-    const val androidGradlePlugin = "com.android.tools.build:gradle:3.5.1"
+    const val androidGradlePlugin = "com.android.tools.build:gradle:3.5.2"
     const val advancedVersioning = "org.moallemi.gradle.advanced-build-version:gradle-plugin:1.6.0"
     const val mvRx = "com.airbnb.android:mvrx:1.3.0"
 
@@ -53,7 +48,7 @@ object Libs {
     }
 
     object Kotlin {
-        private const val version = "1.3.50"
+        private const val version = "1.3.60"
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val reflect = "org.jetbrains.kotlin:kotlin-reflect:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
@@ -61,7 +56,7 @@ object Libs {
     }
 
     object Coroutines {
-        private const val version = "1.3.1"
+        private const val version = "1.3.2"
         const val core = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$version"
         const val rx2 = "org.jetbrains.kotlinx:kotlinx-coroutines-rx2:$version"
         const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
@@ -76,7 +71,7 @@ object Libs {
 
     object AndroidX {
         const val appcompat = "androidx.appcompat:appcompat:1.1.0"
-        const val browser = "androidx.browser:browser:1.0.0"
+        const val browser = "androidx.browser:browser:1.2.0-beta01"
         const val collection = "androidx.collection:collection-ktx:1.1.0"
         const val palette = "androidx.palette:palette:1.0.0"
         const val recyclerview = "androidx.recyclerview:recyclerview:1.1.0-beta04"
@@ -84,10 +79,10 @@ object Libs {
         const val swiperefresh = "androidx.swiperefreshlayout:swiperefreshlayout:1.1.0-alpha02"
         const val preference = "androidx.preference:preference:1.1.0"
         const val constraintlayout = "androidx.constraintlayout:constraintlayout:2.0.0-beta2"
-        const val coreKtx = "androidx.core:core-ktx:1.2.0-alpha04"
+        const val coreKtx = "androidx.core:core-ktx:1.2.0-rc01"
 
         object Navigation {
-            private const val version = "2.2.0-alpha01"
+            private const val version = "2.2.0-rc02"
             const val fragment = "androidx.navigation:navigation-fragment-ktx:$version"
             const val ui = "androidx.navigation:navigation-ui-ktx:$version"
             const val safeArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:$version"
@@ -117,13 +112,13 @@ object Libs {
         }
 
         object Lifecycle {
-            private const val version = "2.2.0-alpha03"
+            private const val version = "2.2.0-rc02"
             const val extensions = "androidx.lifecycle:lifecycle-extensions:$version"
             const val viewmodel = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
         }
 
         object Room {
-            private const val version = "2.2.0-rc01"
+            private const val version = "2.2.2"
             const val common = "androidx.room:room-common:$version"
             const val runtime = "androidx.room:room-runtime:$version"
             const val compiler = "androidx.room:room-compiler:$version"
