55be67ced32b4625547068e8a6525f742b31a05e
Gnoemes <gnoemes@gmail.com>
2019-11-29 20:42:35 +0400

    added base binding fragment

---
+++ common/src/main/java/com/gnoemes/common/BaseBindingFragment.kt
@@ -0,0 +1,40 @@
+package com.gnoemes.common
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseBindingFragment<Binding : ViewDataBinding> : BaseFragment() {
+    private var binding: Binding? = null
+
+    final override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
+        createBinding(inflater, container, savedInstanceState)
+            .also { binding = it }
+            .root
+
+    final override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        onViewCreated(requireBinding(), savedInstanceState)
+    }
+
+    final override fun invalidate() = invalidate(requireBinding())
+
+    protected fun requireBinding(): Binding = requireNotNull(binding)
+
+    protected abstract fun onViewCreated(binding: Binding, savedInstanceState: Bundle?)
+
+    protected abstract fun invalidate(binding: Binding)
+
+    protected abstract fun createBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): Binding
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+}
