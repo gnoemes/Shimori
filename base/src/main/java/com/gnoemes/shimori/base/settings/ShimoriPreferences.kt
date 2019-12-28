6b3fc8c46a70bd382fa8a1e9e6fc87ee3358af34
Gnoemes <gnoemes@gmail.com>
2019-12-28 18:27:00 +0400

    added theme preference

--- base/src/main/java/com/gnoemes/shimori/base/settings/ShimoriPreferences.kt
+++ base/src/main/java/com/gnoemes/shimori/base/settings/ShimoriPreferences.kt
@@ -2,4 +2,13 @@ package com.gnoemes.shimori.base.settings
 
 interface ShimoriPreferences {
     fun setup()
+
+    val themePreference: Theme
+
+    enum class Theme {
+        LIGHT,
+        DARK,
+        BATTERY_SAVER_ONLY,
+        SYSTEM
+    }
 }
