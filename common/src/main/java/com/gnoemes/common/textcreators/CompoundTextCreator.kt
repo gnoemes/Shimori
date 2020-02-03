d5a1100b316d5d7614b176b5f9383ea584e13470
Gnoemes <gnoemes@gmail.com>
2020-02-03 23:44:29 +0400

    added rate progress, refactored typography styles, fixed dimension res

--- common/src/main/java/com/gnoemes/common/textcreators/CompoundTextCreator.kt
+++ common/src/main/java/com/gnoemes/common/textcreators/CompoundTextCreator.kt
@@ -3,6 +3,7 @@ package com.gnoemes.common.textcreators
 import android.content.Context
 import com.gnoemes.shimori.model.ShimoriEntity
 import com.gnoemes.shimori.model.anime.Anime
+import com.gnoemes.shimori.model.rate.Rate
 import javax.inject.Inject
 
 class CompoundTextCreator @Inject constructor(
@@ -17,6 +18,11 @@ class CompoundTextCreator @Inject constructor(
 
     fun rateDescription(entity: ShimoriEntity) = when (entity) {
         is Anime -> animeTextCreator.rateDescription(entity)
+        else -> throw IllegalArgumentException("$entity entity is not supported")
+    }
+
+    fun rateProgress(rate: Rate, entity: ShimoriEntity) = when(entity) {
+        is Anime -> animeTextCreator.rateProgress(rate, entity)
         else -> throw IllegalArgumentException("$entity entity is not supported")
     }
 
