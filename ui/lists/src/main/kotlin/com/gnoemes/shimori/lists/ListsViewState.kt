4ad281f2d08830cdbff1ed97656e1321276ffd2a
gnoemes <gnoemes@gmail.com>
2021-08-01 23:20:43 +0400

    removed unused code

--- ui/lists/src/main/kotlin/com/gnoemes/shimori/lists/ListsViewState.kt
+++ ui/lists/src/main/kotlin/com/gnoemes/shimori/lists/ListsViewState.kt
@@ -15,7 +15,6 @@ internal data class ListsViewState(
     val activeSort: RateSort = RateSort.defaultForType(type),
     val sorts: List<RateSortOption> = RateSortOption.priorityForType(type),
     val user: UserShort? = null,
-    val currentPage : ListsPage = ListsPage.WATCHING,
     val pages: List<ListsPage> = emptyList(),
 ) {
     companion object {
