7fcfa810e11627e420c3bba9fb779fb2c59625a8
gnoemes <gnoemes@gmail.com>
2021-08-10 23:16:08 +0400

    refactored bottom sheet show logic

--- app/src/main/kotlin/com/gnoemes/shimori/main/MainAction.kt
+++ app/src/main/kotlin/com/gnoemes/shimori/main/MainAction.kt
@@ -4,7 +4,5 @@ import com.gnoemes.shimori.model.rate.RateTargetType
 
 internal sealed class MainAction {
     object Random : MainAction()
-    object RateTypeDialog : MainAction()
-    object RateTypeDialogClosed : MainAction()
     data class ChangeRateType(val rateTargetType: RateTargetType) : MainAction()
 }
