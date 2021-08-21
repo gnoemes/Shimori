8120ae836ff1f41e43eb1f486c4842dd2ec9117b
gnoemes <gnoemes@gmail.com>
2021-08-21 20:27:29 +0400

    added custom tab row for full screen width divider support

--- common/src/main/kotlin/com/gnoemes/shimori/common/compose/Pager.kt
+++ common/src/main/kotlin/com/gnoemes/shimori/common/compose/Pager.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.TabPosition
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
