752653759b3f112fdcb45574736239db1d8c3a5e
gnoemes <gnoemes@gmail.com>
2021-08-29 19:18:05 +0400

    updated toolbar

--- common/src/main/kotlin/com/gnoemes/shimori/common/compose/Toolbar.kt
+++ common/src/main/kotlin/com/gnoemes/shimori/common/compose/Toolbar.kt
@@ -1,9 +1,11 @@
 package com.gnoemes.shimori.common.compose
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -13,7 +15,9 @@ import com.gnoemes.shimori.common.R
 import com.gnoemes.shimori.common.compose.theme.ShimoriTheme
 import com.gnoemes.shimori.common.compose.theme.toolbar
 import com.gnoemes.shimori.model.user.UserShort
-import com.google.accompanist.insets.statusBarsPadding
+import com.google.accompanist.insets.LocalWindowInsets
+import com.google.accompanist.insets.rememberInsetsPaddingValues
+import com.google.accompanist.insets.ui.TopAppBar
 
 @Composable
 fun RootScreenToolbar(
@@ -26,40 +30,44 @@ fun RootScreenToolbar(
 ) {
     TopAppBar(
             backgroundColor = MaterialTheme.colors.toolbar,
-            contentPadding = PaddingValues(16.dp),
+            contentPadding = rememberInsetsPaddingValues(
+                    insets = LocalWindowInsets.current.statusBars,
+                    applyStart = true,
+                    applyTop = true,
+                    applyEnd = true
+            ),
             elevation = 0.dp,
             modifier = Modifier
-                .statusBarsPadding()
                 .height(64.dp)
                 .fillMaxWidth(),
-    ) {
+            title = {
+                Text(
+                        text = title,
+                        style = MaterialTheme.typography.h3,
+                        color = MaterialTheme.colors.onPrimary,
+                        modifier = Modifier
+                            .padding(end = 12.dp)
+                )
+            },
+            actions = {
+                if (showSearchButton) {
+                    ShimoriIconButton(
+                            onClick = searchButtonClick,
+                            selected = false,
+                            painter = painterResource(R.drawable.ic_search),
+                            modifier = Modifier
+                                .padding(horizontal = 12.dp)
+                                .size(32.dp),
+                    )
+                }
 
-        Text(
-                text = title,
-                style = MaterialTheme.typography.h3,
-                color = MaterialTheme.colors.onPrimary,
-                modifier = Modifier
-                    .weight(1f)
-                    .padding(end = 12.dp)
-        )
-
-        if (showSearchButton) {
-            ShimoriIconButton(
-                    onClick = searchButtonClick,
-                    selected = false,
-                    painter = painterResource(R.drawable.ic_search),
-                    modifier = Modifier
-                        .padding(horizontal = 12.dp)
-                        .size(32.dp),
-            )
-        }
-
-        UserProfileButton(
-                user = user,
-                authorized = authorized,
-                avatarClick = avatarClick
-        )
-    }
+                UserProfileButton(
+                        user = user,
+                        authorized = authorized,
+                        avatarClick = avatarClick
+                )
+            }
+    )
 }
 
 @Composable
