a5ea2bef71b4ddfeafc72b1af77f5a046d8b2984
gnoemes <gnoemes@gmail.com>
2022-01-16 20:58:26 +0100

    added button components

---
+++ common/src/main/kotlin/com/gnoemes/shimori/common/compose/theme/Shapes.kt
@@ -0,0 +1,8 @@
+package com.gnoemes.shimori.common.compose.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.unit.dp
+
+val ShimoriDefaultRoundedCornerShape = RoundedCornerShape(12.dp)
+val ShimoriBigRoundedCornerShape = RoundedCornerShape(16.dp)
+val ShimoriSmallRoundedCornerShape = RoundedCornerShape(8.dp)
