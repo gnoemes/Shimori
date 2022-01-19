4d2beb19cdb69887c01e6eb492a7a85c70a7942d
gnoemes <gnoemes@gmail.com>
2022-01-19 22:05:09 +0100

    deleted lists

--- ui/lists/src/main/kotlin/com/gnoemes/shimori/lists/tabs/page/ListPageAction.kt
+++
@@ -1,7 +0,0 @@
-package com.gnoemes.shimori.lists.tabs.page
-
-import com.gnoemes.shimori.model.rate.RateTargetType
-
-internal sealed class ListPageAction {
-    data class TogglePin(val id : Long, val targetType : RateTargetType) : ListPageAction()
-}
