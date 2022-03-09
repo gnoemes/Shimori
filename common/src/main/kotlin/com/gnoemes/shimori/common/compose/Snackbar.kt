fd38e216d6216831cb9ae7af117abc9d3191822e
gnoemes <gnoemes@gmail.com>
2022-03-10 00:48:29 +0100

    added snackbar & text provider

---
+++ common/src/main/kotlin/com/gnoemes/shimori/common/compose/Snackbar.kt
@@ -0,0 +1,72 @@
+package com.gnoemes.shimori.common.compose
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarData
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.gnoemes.shimori.common.compose.theme.ShimoriSmallRoundedCornerShape
+
+@Composable
+fun ShimoriSnackbar(
+    hostState: SnackbarHostState,
+    modifier: Modifier = Modifier,
+    onDismiss: () -> Unit = { hostState.currentSnackbarData?.dismiss() },
+    snackbar: @Composable (SnackbarData) -> Unit = { data ->
+        SwipeDismissSnackbar(
+            data = data,
+            onDismiss = onDismiss,
+        )
+    }
+) {
+    SnackbarHost(
+        hostState = hostState,
+        modifier = modifier,
+        snackbar = snackbar
+    )
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun SwipeDismissSnackbar(
+    data: SnackbarData,
+    onDismiss: (() -> Unit)? = null,
+    snackbar: @Composable (SnackbarData) -> Unit = {
+        Snackbar(
+            modifier = Modifier.padding(8.dp),
+            shape = ShimoriSmallRoundedCornerShape
+        ) {
+            Text(
+                text = it.visuals.message,
+            )
+        }
+    },
+) {
+    val dismissState = rememberDismissState {
+        if (it != DismissValue.Default) {
+            // First dismiss the snackbar
+            data.dismiss()
+            // Then invoke the callback
+            onDismiss?.invoke()
+        }
+        true
+    }
+
+    SwipeToDismiss(
+        state = dismissState,
+        directions = setOf(DismissDirection.StartToEnd, DismissDirection.EndToStart),
+        background = {},
+        dismissContent = { snackbar(data) }
+    )
+}
+
+@Composable
+fun rememberSnackbarHostState() = remember {
+    SnackbarHostState()
+}
