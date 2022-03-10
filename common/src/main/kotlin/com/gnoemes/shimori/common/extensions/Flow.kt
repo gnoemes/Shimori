c8ff20b56568a9d34e8921b54231e224d57cbc27
gnoemes <gnoemes@gmail.com>
2022-03-10 20:47:59 +0100

    added lists screen & empty state

--- common/src/main/kotlin/com/gnoemes/shimori/common/extensions/Flow.kt
+++ common/src/main/kotlin/com/gnoemes/shimori/common/extensions/Flow.kt
@@ -18,14 +18,44 @@
 
 package com.gnoemes.shimori.common.extensions
 
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.repeatOnLifecycle
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+
+@Composable
+fun <T> rememberFlowWithLifecycle(
+    flow: Flow<T>,
+    lifecycle: Lifecycle = LocalLifecycleOwner.current.lifecycle,
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED
+): Flow<T> = remember(flow, lifecycle) {
+    flow.flowWithLifecycle(
+        lifecycle = lifecycle,
+        minActiveState = minActiveState
+    )
+}
+
+@Composable
+fun <T> rememberStateWithLifecycle(
+    stateFlow: StateFlow<T>,
+    lifecycle: Lifecycle = LocalLifecycleOwner.current.lifecycle,
+    minActiveState: Lifecycle.State = Lifecycle.State.STARTED
+): State<T> {
+    val initialValue = remember(stateFlow) { stateFlow.value }
+    return produceState(
+        key1 = stateFlow, key2 = lifecycle, key3 = minActiveState,
+        initialValue = initialValue
+    ) {
+        lifecycle.repeatOnLifecycle(minActiveState) {
+            stateFlow.collect {
+                this@produceState.value = it
+            }
+        }
+    }
+}
 
 @Composable
 fun <T> Flow<T>.collectAsStateWithLifecycle(
