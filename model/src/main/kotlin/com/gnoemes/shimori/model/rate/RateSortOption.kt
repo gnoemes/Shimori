0c7237c817d6899dd8ff010ab22f7db3af2c5012
gnoemes <gnoemes@gmail.com>
2022-03-16 20:37:43 +0100

    added sorts to pinned list

--- model/src/main/kotlin/com/gnoemes/shimori/model/rate/RateSortOption.kt
+++ model/src/main/kotlin/com/gnoemes/shimori/model/rate/RateSortOption.kt
@@ -3,16 +3,22 @@ package com.gnoemes.shimori.model.rate
 enum class RateSortOption {
     NAME,
     PROGRESS,
+
     //Last released
     DATE_AIRED,
+
     //rating from Rate.kt "Your Score"
     MY_SCORE,
+
     //rating from Entity "User's score"
     RATING,
+
     //episodes, chapters etc
     SIZE,
+
     //Last added
     DATE_CREATED,
+
     //Last changed
     DATE_UPDATED,
 
@@ -20,23 +26,32 @@ enum class RateSortOption {
 
     companion object {
         private val default = listOf(
-                PROGRESS,
-                SIZE,
-                DATE_UPDATED,
-                DATE_CREATED,
-                DATE_AIRED,
-                NAME,
-                MY_SCORE,
-                RATING,
+            PROGRESS,
+            SIZE,
+            DATE_UPDATED,
+            DATE_CREATED,
+            DATE_AIRED,
+            NAME,
+            MY_SCORE,
+            RATING,
         )
         val animePriorityValues: List<RateSortOption> = default
         val mangaPriorityValues: List<RateSortOption> = default
+        val pinnedPriorityValues: List<RateSortOption> = listOf(
+            PROGRESS,
+            DATE_UPDATED,
+            DATE_CREATED,
+            DATE_AIRED,
+            NAME,
+            MY_SCORE,
+            RATING,
+        )
 
         fun priorityForType(type: ListType): List<RateSortOption> = when (type) {
             ListType.Anime -> animePriorityValues
             ListType.Manga -> mangaPriorityValues
             ListType.Ranobe -> default
-            ListType.Pinned -> emptyList()
+            ListType.Pinned -> pinnedPriorityValues
             else -> default
         }
     }
