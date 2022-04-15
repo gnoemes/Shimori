f0f728befd3867b696b177b8c28af558119a33b9
gnoemes <gnoemes@gmail.com>
2022-04-15 20:14:40 +0200

    WIP: configured build.gradle files

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,15 @@
 plugins {
     `kotlin-dsl`
 }
+
 repositories {
-    jcenter()
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+dependencies {
+    api("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.20.0-RC2")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    api("com.android.tools.build:gradle:7.3.0-alpha08")
 }
