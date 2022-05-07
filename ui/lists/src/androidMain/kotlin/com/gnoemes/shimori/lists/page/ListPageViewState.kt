754362acdd2c87ee8c5037beebce117969307a33
gnoemes <gnoemes@gmail.com>
2022-05-07 21:15:17 +0200

    added list page

---
+++ ui/lists/src/androidMain/kotlin/com/gnoemes/shimori/lists/page/ListPageViewState.kt
@@ -0,0 +1,17 @@
+package com.gnoemes.shimori.lists.page
+
+import androidx.compose.runtime.Immutable
+import com.gnoemes.shimori.data.core.entities.rate.ListType
+import com.gnoemes.shimori.data.core.entities.rate.RateStatus
+import com.gnoemes.shimori.data.core.entities.user.UserShort
+
+@Immutable
+internal data class ListPageViewState(
+    val type: ListType = ListType.Anime,
+    val status: RateStatus = RateStatus.WATCHING,
+    val user: UserShort? = null,
+) {
+    companion object {
+        val Empty = ListPageViewState()
+    }
+}
