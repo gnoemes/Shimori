2d6d28ce34c53a0c0c4b9729c650b70613f5da80
gnoemes <gnoemes@gmail.com>
2022-05-11 19:47:58 +0200

    use transparent status bar & toolbar with space

--- common-ui/src/androidMain/kotlin/com/gnoemes/shimori/common/ui/Modifier.kt
+++ common-ui/src/androidMain/kotlin/com/gnoemes/shimori/common/ui/Modifier.kt
@@ -2,6 +2,10 @@ package com.gnoemes.shimori.common.ui
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
@@ -10,4 +14,9 @@ inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier
     clickable(
         indication = null,
         interactionSource = remember { MutableInteractionSource() }) { onClick() }
+}
+
+fun Modifier.statusBarHeight(): Modifier = composed {
+    val statusBarHeight = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
+    height(statusBarHeight)
 }
