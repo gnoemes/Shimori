ff32903733f7808ba2ba8c0ee7cd9d92e6ac8b37
gnoemes <gnoemes@gmail.com>
2022-08-07 18:57:23 +0200

    added shikimori platform values entity

--- base/core/src/main/kotlin/com/gnoemes/shimori/base/core/entities/Platform.kt
+++ base/core/src/main/kotlin/com/gnoemes/shimori/base/core/entities/Platform.kt
@@ -1,14 +1,10 @@
 package com.gnoemes.shimori.base.core.entities
 
 data class Platform(
-    val type : Type,
-    val debug : Boolean,
+    val type: Type,
+    val debug: Boolean,
     val appVersion: String = "",
-    val shikimoriURL: String = "",
-    val shikimoriClientId: String = "",
-    val shikimoriSecretKey: String = "",
-    val shikimoriRedirect: String = "",
-    val shikimoriUserAgent: String = ""
+    val shikimori: ShikimoriPlatformValues
 ) {
     enum class Type {
         Android, Jvm, Ios
---
+++ base/core/src/main/kotlin/com/gnoemes/shimori/base/core/entities/ShikimoriPlatformValues.kt
@@ -0,0 +1,9 @@
+package com.gnoemes.shimori.base.core.entities
+
+data class ShikimoriPlatformValues(
+    val url: String = "",
+    val clientId: String = "",
+    val secretKey: String = "",
+    val oauthRedirect: String = "",
+    val userAgent: String = ""
+)
