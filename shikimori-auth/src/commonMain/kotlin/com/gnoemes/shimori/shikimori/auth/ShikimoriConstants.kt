1788db871e7c32bdd0758bf0d239cc7d302c90fe
gnoemes <gnoemes@gmail.com>
2022-08-08 21:53:41 +0200

    show snackbar if app was restricted in shikimori by user

--- shikimori-auth/src/commonMain/kotlin/com/gnoemes/shimori/shikimori/auth/ShikimoriConstants.kt
+++ shikimori-auth/src/commonMain/kotlin/com/gnoemes/shimori/shikimori/auth/ShikimoriConstants.kt
@@ -4,5 +4,7 @@ object ShikimoriConstants {
     const val SIGN_IN_ENDPOINT = "https://shikimori.one/users/sign_in"
     const val SIGN_UP_ENDPOINT = "https://shikimori.one/users/sign_up"
     const val OAUTH_ENDPOINT = "https://shikimori.one/oauth/authorize"
-    const val CODE_PATTERN = "(code|error)=(.*)"
+    const val CODE_PATTERN = "(code|error)=(.*?)(?:&|$)"
+
+    const val ERROR_REASON_ACCESS_DENIED = "access_denied"
 }
