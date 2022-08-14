96dd3acbcaa5d03c88d7dc7a50eccb8b2c598ec9
gnoemes <gnoemes@gmail.com>
2022-08-14 16:28:05 +0200

    fixed token update

--- shikimori/src/main/kotlin/com/gnoemes/shikimori/entities/auth/TokenResponse.kt
+++ shikimori/src/main/kotlin/com/gnoemes/shikimori/entities/auth/TokenResponse.kt
@@ -4,6 +4,8 @@ import kotlinx.serialization.SerialName
 
 @kotlinx.serialization.Serializable
 data class TokenResponse(
-    @SerialName("access_token") val accessToken: String,
-    @SerialName("refresh_token") val refreshToken: String
-)
+    @SerialName("access_token") val accessToken: String?,
+    @SerialName("refresh_token") val refreshToken: String?
+) {
+    val isEmpty get() = accessToken == null || refreshToken == null
+}
