35a17de3b255ebd0c942f42c18c088d331c52600
gnoemes <gnoemes@gmail.com>
2022-08-15 19:50:27 +0200

    added minTouchTarget size modifier extension

--- common-ui/src/androidMain/kotlin/com/gnoemes/shimori/common/ui/Modifier.kt
+++ common-ui/src/androidMain/kotlin/com/gnoemes/shimori/common/ui/Modifier.kt
@@ -6,9 +6,20 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.statusBars
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalMinimumTouchTargetEnforcement
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.layout.LayoutModifier
+import androidx.compose.ui.layout.Measurable
+import androidx.compose.ui.layout.MeasureResult
+import androidx.compose.ui.layout.MeasureScope
+import androidx.compose.ui.platform.debugInspectorInfo
+import androidx.compose.ui.unit.Constraints
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
 
 inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier = composed {
     clickable(
@@ -19,4 +30,50 @@ inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier
 fun Modifier.statusBarHeight(): Modifier = composed {
     val statusBarHeight = WindowInsets.statusBars.asPaddingValues().calculateTopPadding()
     height(statusBarHeight)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Suppress("ModifierInspectorInfo")
+fun Modifier.minimumTouchTargetSize(): Modifier = composed(
+    inspectorInfo = debugInspectorInfo {
+        name = "minimumTouchTargetSize"
+
+        properties["README"] = "Adds outer padding to measure at least 48.dp (default) in " +
+                "size to disambiguate touch interactions if the element would measure smaller"
+    }
+) {
+    if (LocalMinimumTouchTargetEnforcement.current) {
+        MinimumTouchTargetModifier(DpSize(48.dp, 48.dp))
+    } else {
+        Modifier
+    }
+}
+
+private class MinimumTouchTargetModifier(val size: DpSize) : LayoutModifier {
+    override fun MeasureScope.measure(
+        measurable: Measurable,
+        constraints: Constraints
+    ): MeasureResult {
+
+        val placeable = measurable.measure(constraints)
+
+        // Be at least as big as the minimum dimension in both dimensions
+        val width = maxOf(placeable.width, size.width.roundToPx())
+        val height = maxOf(placeable.height, size.height.roundToPx())
+
+        return layout(width, height) {
+            val centerX = ((width - placeable.width) / 2f).roundToInt()
+            val centerY = ((height - placeable.height) / 2f).roundToInt()
+            placeable.place(centerX, centerY)
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        val otherModifier = other as? MinimumTouchTargetModifier ?: return false
+        return size == otherModifier.size
+    }
+
+    override fun hashCode(): Int {
+        return size.hashCode()
+    }
 }
