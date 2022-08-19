7fdab658aba23c074b125459ce6352a968c72e6e
gnoemes <gnoemes@gmail.com>
2022-08-19 17:51:15 +0200

    fixed color generation

--- common-ui/src/androidMain/kotlin/com/gnoemes/shimori/common/ui/utils/PaletteBuilder.kt
+++ common-ui/src/androidMain/kotlin/com/gnoemes/shimori/common/ui/utils/PaletteBuilder.kt
@@ -3,6 +3,7 @@ package com.gnoemes.shimori.common.ui.utils
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import com.gnoemes.shimori.common.ui.theme.*
+import kotlin.math.max
 
 /**
  * Dynamic color-like palette generation but for given primary accent color.
@@ -31,185 +32,81 @@ fun generateDarkPaletteForPrimary(target: Color): AppPalette {
 /**
  * Generates accent and neutral colors
  * @see <a href=https://m3.material.io/styles/color/the-color-system/key-colors-tones>MD3 Color system</a>
- * @return [AccentsPalette]
+ * @return [CorePalette]
  */
-private fun generateAccents(target: Color): AccentsPalette {
-    val a1 = Hct.fromInt(target.toArgb())
-    val a2 = a1.createAccent(a1.getHue(), 16f)
-    val a3 = a1.createAccent(a1.getHue() + 60, 24f)
-    val n1 = a1.createAccent(a1.getHue(), 4f)
-    val n2 = a1.createAccent(a1.getHue(), 8f)
-    return AccentsPalette(a1, a2, a3, n1, n2)
+private fun generateAccents(target: Color): CorePalette {
+    return CorePalette.from(target.toArgb())
 }
 
 private fun createPalette(
-    palette: AccentsPalette,
+    palette: CorePalette,
     light: Boolean
 ): AppPalette {
     return object : AppPalette {
-        override val primary = primary(palette.primary, light)
-        override val onPrimary = onPrimary(palette.primary, light)
-        override val primaryContainer = primaryContainer(palette.primary, light)
-        override val onPrimaryContainer = onPrimaryContainer(palette.primary, light)
-        override val inversePrimary = primaryInverse(palette.primary, light)
-        override val secondary = secondary(palette.secondary, light)
-        override val onSecondary = onSecondary(palette.secondary, light)
-        override val secondaryContainer = secondaryContainer(palette.secondary, light)
-        override val onSecondaryContainer = onSecondaryContainer(palette.secondary, light)
-        override val tertiary = tertiary(palette.tertiary, light)
-        override val onTertiary = onTertiary(palette.tertiary, light)
-        override val tertiaryContainer = tertiaryContainer(palette.tertiary, light)
-        override val onTertiaryContainer = onTertiaryContainer(palette.tertiary, light)
+        override val primary = palette.primary.tone(if (light) 40 else 80).color()
+        override val onPrimary = palette.primary.tone(if (light) 100 else 20).color()
+        override val primaryContainer = palette.primary.tone(if (light) 90 else 30).color()
+        override val onPrimaryContainer = palette.primary.tone(if (light) 10 else 90).color()
+        override val inversePrimary = palette.primary.tone(if (light) 80 else 40).color()
+        override val secondary = palette.secondary.tone(if (light) 40 else 80).color()
+        override val onSecondary = palette.secondary.tone(if (light) 100 else 20).color()
+        override val secondaryContainer = palette.secondary.tone(if (light) 90 else 30).color()
+        override val onSecondaryContainer = palette.secondary.tone(if (light) 10 else 90).color()
+        override val tertiary = palette.tertiary.tone(if (light) 40 else 80).color()
+        override val onTertiary = palette.tertiary.tone(if (light) 100 else 20).color()
+        override val tertiaryContainer = palette.tertiary.tone(if (light) 90 else 30).color()
+        override val onTertiaryContainer = palette.tertiary.tone(if (light) 10 else 90).color()
         override val error = if (light) lightError else darkError
         override val onError = if (light) lightOnError else darkOnError
         override val errorContainer = if (light) lightErrorContainer else darkErrorContainer
         override val onErrorContainer = if (light) lightOnErrorContainer else darkOnErrorContainer
-        override val background = background(palette.neutral, light)
-        override val onBackground = onBackground(palette.neutral, light)
-        override val surface = surface(palette.neutral, light)
-        override val onSurface = onSurface(palette.neutral, light)
-        override val surfaceTint = primary(palette.primary, light).copy(alpha = 0.05f)
-        override val inverseSurface = inverseSurface(palette.neutral, light)
-        override val inverseOnSurface = inverseOnSurface(palette.neutral, light)
-        override val surfaceVariant = surfaceVariant(palette.neutralVariant, light)
-        override val onSurfaceVariant = onSurfaceVariant(palette.neutralVariant, light)
-        override val outline = outline(palette.neutralVariant, light)
-        override val outlineVariant = outlineVariant(palette.neutralVariant, light)
-        override val scrim = scrim(palette.neutral, light).copy(alpha = .32f)
+        override val background = palette.neutral.tone(if (light) 99 else 10).color()
+        override val onBackground = palette.neutral.tone(if (light) 10 else 90).color()
+        override val surface = palette.neutral.tone(if (light) 99 else 10).color()
+        override val onSurface = palette.neutral.tone(if (light) 10 else 90).color()
+        override val surfaceTint = primary.copy(alpha = 0.05f)
+        override val inverseSurface = palette.neutral.tone(if (light) 20 else 90).color()
+        override val inverseOnSurface = palette.neutral.tone(if (light) 95 else 10).color()
+        override val surfaceVariant = palette.neutralVariant.tone(if (light) 90 else 30).color()
+        override val onSurfaceVariant = palette.neutralVariant.tone(if (light) 30 else 80).color()
+        override val outline = palette.neutralVariant.tone(if (light) 50 else 60).color()
+        override val outlineVariant = palette.neutralVariant.tone(if (light) 80 else 30).color()
+        override val scrim = palette.neutral.tone(0).color().copy(alpha = .32f)
     }
 }
 
-private fun primary(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 40f else 80f)
-    return hct.color()
+private class CorePalette(
+    val primary: TonalPalette,
+    val secondary: TonalPalette,
+    val tertiary: TonalPalette,
+    val neutral: TonalPalette,
+    val neutralVariant: TonalPalette,
+) {
+
+    companion object {
+        internal fun from(argb: Int): CorePalette {
+            val hct = Hct.fromInt(argb)
+            val hue = hct.getHue()
+            val chroma = hct.getChroma()
+
+            return CorePalette(
+                TonalPalette(hue, max(40f, chroma)),
+                TonalPalette(hue, 16f),
+                TonalPalette(hue + 60f, 24f),
+                TonalPalette(hue, 4f),
+                TonalPalette(hue, 8f)
+            )
+        }
+    }
 }
 
-private fun onPrimary(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 100f else 20f)
-    return hct.color()
+private data class TonalPalette(
+    val hue: Float,
+    val chroma: Float
+) {
+    fun tone(tone: Int): Int {
+        return Hct.from(this.hue, this.chroma, tone.toFloat()).toInt()
+    }
 }
 
-private fun primaryContainer(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 90f else 30f)
-    return hct.color()
-}
-
-private fun onPrimaryContainer(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 10f else 90f)
-    return hct.color()
-}
-
-private fun primaryInverse(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 80f else 40f)
-    return hct.color()
-}
-
-private fun secondary(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 40f else 80f)
-    return hct.color()
-}
-
-private fun onSecondary(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 100f else 20f)
-    return hct.color()
-}
-
-private fun secondaryContainer(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 90f else 30f)
-    return hct.color()
-}
-
-private fun onSecondaryContainer(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 10f else 90f)
-    return hct.color()
-}
-
-private fun tertiary(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 40f else 80f)
-    return hct.color()
-}
-
-private fun onTertiary(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 100f else 20f)
-    return hct.color()
-}
-
-private fun tertiaryContainer(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 90f else 30f)
-    return hct.color()
-}
-
-private fun onTertiaryContainer(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 10f else 90f)
-    return hct.color()
-}
-
-private fun background(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 99f else 10f)
-    return hct.color()
-}
-
-private fun onBackground(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 10f else 90f)
-    return hct.color()
-}
-
-private fun surface(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 99f else 10f)
-    return hct.color()
-}
-
-private fun onSurface(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 10f else 90f)
-    return hct.color()
-}
-
-private fun surfaceVariant(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 90f else 30f)
-    return hct.color()
-}
-
-private fun onSurfaceVariant(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 30f else 80f)
-    return hct.color()
-}
-
-private fun outline(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 50f else 60f)
-    return hct.color()
-}
-
-private fun outlineVariant(hct: Hct, light: Boolean) : Color {
-    hct.setTone(if (light) 80f else 30f)
-    return hct.color()
-}
-
-private fun scrim(hct: Hct, light: Boolean) : Color {
-    hct.setTone(0f)
-    return hct.color()
-}
-
-private fun inverseSurface(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 20f else 90f)
-    return hct.color()
-}
-
-private fun inverseOnSurface(hct: Hct, light: Boolean): Color {
-    hct.setTone(if (light) 95f else 10f)
-    return hct.color()
-}
-
-
-private fun Hct.createAccent(newHue: Float, newChroma: Float): Hct {
-    return Hct.from(newHue, newChroma, this.getTone())
-}
-
-private data class AccentsPalette(
-    val primary: Hct,
-    val secondary: Hct,
-    val tertiary: Hct,
-    val neutral: Hct,
-    val neutralVariant: Hct,
-)
-
-private fun Hct.color() = Color(toInt())
+private fun Int.color() = Color(this)
