615c646f226dd7048fb7aea44678e6033cfd01fe
gnoemes <gnoemes@gmail.com>
2022-08-25 23:20:23 +0200

    added manga & ranobe shikimori details

--- shikimori/src/main/kotlin/com/gnoemes/shikimori/sources/ShikimoriMangaDataSource.kt
+++ shikimori/src/main/kotlin/com/gnoemes/shikimori/sources/ShikimoriMangaDataSource.kt
@@ -1,10 +1,11 @@
 package com.gnoemes.shikimori.sources
 
 import com.gnoemes.shikimori.Shikimori
-import com.gnoemes.shikimori.mappers.manga.MangaResponseMapper
+import com.gnoemes.shikimori.mappers.manga.MangaDetailsMapper
 import com.gnoemes.shikimori.mappers.manga.RateResponseToMangaWithRateMapper
 import com.gnoemes.shikimori.mappers.rate.RateStatusMapper
 import com.gnoemes.shimori.data.core.entities.rate.RateStatus
+import com.gnoemes.shimori.data.core.entities.titles.manga.Manga
 import com.gnoemes.shimori.data.core.entities.titles.manga.MangaWithRate
 import com.gnoemes.shimori.data.core.entities.user.UserShort
 import com.gnoemes.shimori.data.core.mappers.forLists
@@ -14,11 +15,17 @@ internal class ShikimoriMangaDataSource(
     private val shikimori: Shikimori,
     private val ratedMapper: RateResponseToMangaWithRateMapper,
     private val statusMapper: RateStatusMapper,
+    private val detailsMapper: MangaDetailsMapper,
 ) : MangaDataSource {
 
     override suspend fun getWithStatus(user: UserShort, status: RateStatus?): List<MangaWithRate> {
         return shikimori.manga.getUserRates(user.shikimoriId, statusMapper.mapInverse(status))
             .let { ratedMapper.forLists().invoke(it) }
             .filter { it.entity.mangaType != null }
+    }
+
+    override suspend fun get(title: Manga): MangaWithRate {
+        return shikimori.manga.getDetails(title.shikimoriId)
+            .let { detailsMapper.map(it) }
     }
 }
--- shikimori/src/main/kotlin/com/gnoemes/shikimori/sources/ShikimoriRanobeDataSource.kt
+++ shikimori/src/main/kotlin/com/gnoemes/shikimori/sources/ShikimoriRanobeDataSource.kt
@@ -1,7 +1,7 @@
 package com.gnoemes.shikimori.sources
 
 import com.gnoemes.shikimori.Shikimori
-import com.gnoemes.shikimori.mappers.ranobe.RanobeResponseMapper
+import com.gnoemes.shikimori.mappers.ranobe.RanobeDetailsMapper
 import com.gnoemes.shikimori.mappers.ranobe.RateResponseToRanobeWithRateMapper
 import com.gnoemes.shikimori.mappers.rate.RateStatusMapper
 import com.gnoemes.shimori.data.core.entities.rate.RateStatus
@@ -15,11 +15,17 @@ internal class ShikimoriRanobeDataSource(
     private val shikimori: Shikimori,
     private val ratedMapper: RateResponseToRanobeWithRateMapper,
     private val statusMapper: RateStatusMapper,
+    private val detailsMapper: RanobeDetailsMapper,
 ) : RanobeDataSource {
 
     override suspend fun getWithStatus(user: UserShort, status: RateStatus?): List<RanobeWithRate> {
         return shikimori.ranobe.getUserRates(user.shikimoriId, statusMapper.mapInverse(status))
             .let { ratedMapper.forLists().invoke(it) }
             .filter { it.entity.ranobeType != null }
+    }
+
+    override suspend fun get(title: Ranobe): RanobeWithRate {
+        return shikimori.manga.getDetails(title.shikimoriId)
+            .let { detailsMapper.map(it) }
     }
 }
