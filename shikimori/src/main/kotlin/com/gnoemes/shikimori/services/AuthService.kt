bc0f1a6a54060f799f9c37d08682b861ac481203
gnoemes <gnoemes@gmail.com>
2022-08-25 19:57:28 +0200

    possible fix for ktor token refresh deadlock

--- shikimori/src/main/kotlin/com/gnoemes/shikimori/services/AuthService.kt
+++ shikimori/src/main/kotlin/com/gnoemes/shikimori/services/AuthService.kt
@@ -1,8 +1,9 @@
 package com.gnoemes.shikimori.services
 
 import com.gnoemes.shikimori.entities.auth.TokenResponse
+import io.ktor.client.*
 
 internal interface AuthService {
     suspend fun accessToken(authCode : String) : TokenResponse?
-    suspend fun refreshToken(refreshToken: String): TokenResponse?
+    suspend fun HttpClient.refreshToken(refreshToken: String): TokenResponse?
 }
