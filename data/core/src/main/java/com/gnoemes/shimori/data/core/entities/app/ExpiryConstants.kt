59b31af474cb9f72370382f86dcfbbaebb4fb9b3
gnoemes <gnoemes@gmail.com>
2022-08-25 23:02:19 +0200

    added expiry constants

---
+++ data/core/src/main/java/com/gnoemes/shimori/data/core/entities/app/ExpiryConstants.kt
@@ -0,0 +1,8 @@
+package com.gnoemes.shimori.data.core.entities.app
+
+//minutes
+object ExpiryConstants {
+    const val TitlesWithStatus = 15
+    const val TitleDetails = 24 * 60
+    const val SyncPendingRates = 5
+}
