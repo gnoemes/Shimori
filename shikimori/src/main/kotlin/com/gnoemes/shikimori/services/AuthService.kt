04fffea765c6ad6245bc34ab1a7d83468df3f7b9
gnoemes <gnoemes@gmail.com>
2022-08-27 19:47:47 +0200

    fixed shikimori auth token refresh freeze

--- shikimori/src/main/kotlin/com/gnoemes/shikimori/services/AuthService.kt
+++ shikimori/src/main/kotlin/com/gnoemes/shikimori/services/AuthService.kt
@@ -1,9 +1,12 @@
 package com.gnoemes.shikimori.services
 
 import com.gnoemes.shikimori.entities.auth.TokenResponse
-import io.ktor.client.*
+import io.ktor.client.request.*
 
 internal interface AuthService {
-    suspend fun accessToken(authCode : String) : TokenResponse?
-    suspend fun HttpClient.refreshToken(refreshToken: String): TokenResponse?
+    suspend fun accessToken(authCode: String): TokenResponse?
+    suspend fun refreshToken(
+        refreshToken: String,
+        block: HttpRequestBuilder.() -> Unit
+    ): TokenResponse?
 }
