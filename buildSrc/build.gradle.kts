e40852aee5d14902babfc035e4fd2ebc473a48c8
gnoemes <gnoemes@gmail.com>
2022-10-20 21:50:45 +0200

    updated gradle

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,5 +12,5 @@ dependencies {
     api("dev.icerock:mobile-multiplatform:0.14.1")
     api("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.20.0-RC2")
     api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
-    api("com.android.tools.build:gradle:7.4.0-alpha09")
+    api("com.android.tools.build:gradle:7.4.0-beta02")
 }
