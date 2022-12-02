a5fabc63a6dcf8b4bb39881d5ac2cd192ac7fd47
gnoemes <gnoemes@gmail.com>
2022-12-02 15:22:10 +0100

    updated compose kotlin compiler

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,6 +11,6 @@ repositories {
 dependencies {
     api("dev.icerock:mobile-multiplatform:0.14.1")
     api("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.20.0-RC2")
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.21")
     api("com.android.tools.build:gradle:7.4.0-beta02")
 }
