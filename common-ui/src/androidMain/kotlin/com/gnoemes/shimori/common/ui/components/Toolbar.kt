97e23be29871a74d0fc67e31aa7dbc60d0f872d3
gnoemes <gnoemes@gmail.com>
2023-04-16 22:38:07 +0200

    fixed max lines for toolbar

--- common-ui/src/androidMain/kotlin/com/gnoemes/shimori/common/ui/components/Toolbar.kt
+++ common-ui/src/androidMain/kotlin/com/gnoemes/shimori/common/ui/components/Toolbar.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
@@ -114,13 +115,20 @@ fun ShimoriSecondaryToolbar(
         TopAppBar(
             title = {
                 if (subTitle == null) {
-                    Text(text = title)
+                    Text(
+                        text = title,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis,
+                    )
                 } else {
                     Column(
                         modifier = Modifier.padding(start = 12.dp)
                     ) {
                         Text(
-                            text = title, style = MaterialTheme.typography.titleMedium
+                            text = title,
+                            style = MaterialTheme.typography.titleMedium,
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis,
                         )
 
                         Text(
