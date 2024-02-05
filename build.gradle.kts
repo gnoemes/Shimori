19b680eb82d798d21e7920a4102dd46ac065057e
gnoemes <gnoemes@gmail.com>
2024-02-05 01:33:04 +0100

    update libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,16 @@ plugins {
     alias(composelibs.plugins.multiplatform) apply false
 }
 
+buildscript {
+    dependencies {
+        // https://github.com/gmazzo/gradle-buildconfig-plugin/issues/131
+        // Yuck. Need to force kotlinpoet:1.16.0 as that is what buildconfig uses.
+        // CMP 1.6.0-x uses kotlinpoet:1.14.x. Gradle seems to force 1.14.x which then breaks
+        // buildconfig.
+        classpath("com.squareup:kotlinpoet:1.16.0")
+    }
+}
+
 allprojects {
     tasks.withType<KotlinCompilationTask<*>>().configureEach {
         compilerOptions {
