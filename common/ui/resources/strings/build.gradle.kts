58c43ce9aa5fd9de0d614adcb3dea0caca4eadf8
gnoemes <gnoemes@gmail.com>
2024-02-09 17:32:16 +0100

    temporary migrate to Moko Resources

--- common/ui/resources/strings/build.gradle.kts
+++ common/ui/resources/strings/build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     id("com.gnoemes.shimori.android.library")
     id("com.gnoemes.shimori.kotlin.multiplatform")
     id("com.gnoemes.shimori.compose")
+
+    //TODO migrate to Compose Resources
+    //Compose resources doesn't support multi module projects right now. Making common:ui:resources basically useless
+    //https://github.com/JetBrains/compose-multiplatform/issues/4083
+    //Temporary replacing this functionality with lib
+    id("dev.icerock.mobile.multiplatform-resources")
 }
 
 kotlin {
@@ -12,7 +18,11 @@ kotlin {
                 api(projects.data.models)
 
                 implementation(compose.runtime)
-                implementation(compose.components.resources)
+//                implementation(compose.components.resources)
+
+                //TODO migrate to Compose Resources
+                api("dev.icerock.moko:resources:0.24.0-alpha-3")
+                api("dev.icerock.moko:resources-compose:0.24.0-alpha-3") // for compose multiplatform
             }
         }
     }
@@ -24,4 +34,9 @@ android {
     sourceSets["main"].apply {
         res.srcDirs("src/androidMain/res", "src/commonMain/resources")
     }
+}
+
+multiplatformResources {
+    resourcesPackage.set("com.gnoemes.shimori")
+    resourcesClassName.set("Res")
 }
